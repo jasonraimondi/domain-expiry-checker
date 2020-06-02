0809b6451593acc4dd8560ad868b773e2c3582d4
Jason Raimondi <jason@raimondi.us>
2020-06-01 23:47:06 -0700

    Add query and parse expiry

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jasonraimondi/domain-expiry-check
+
+go 1.14
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+
+    "github.com/jasonraimondi/domain-expiry-check/whois"
+)
 
 func main() {
-    fmt.Println("Hi Jason");
+    res, err := whois.QueryHost("jasonraimondi.com")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    time_string := res.Output["Registry Expiry Date"][0]
+    t, _ := time.Parse(time.RFC3339, time_string)
+    now := time.Now()
+
+    duration, _ := time.ParseDuration("168h")
+
+    fmt.Println(t, duration)
+    t = t.Add(duration)
+    fmt.Println(t)
+    if now.Unix() > t.Unix() {
+        fmt.Println("yes, the domain is almost expiring")
+    } else {
+        fmt.Println("no, we have a while to go")
+    }
 }
